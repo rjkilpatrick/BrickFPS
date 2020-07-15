71df3c4b91ac958efada3445b7e507306d156687
John Kilpatrick <john.kilpatrick@outlook.com>
2020-07-15 15:15:33 +0100

    Chore: Remove unnecessary  folder

--- godot/player/player.gd
+++ godot/player/player.gd
@@ -4,16 +4,30 @@ var paused = false
 
 # -----------------------
 # Movement
+enum { # TODO: Implement movement state machine
+	IDLE,
+	WALK,
+	SPRINT,
+	CROUCH,
+	JUMP,
+}
 
 onready var GRAVITY = ProjectSettings.get_setting("physics/3d/default_gravity") * ProjectSettings.get_setting("physics/3d/default_gravity_vector")
 export var DEFAULT_MOVE_SPEED = 10
 onready var JUMP_SPEED = 0.5 * ProjectSettings.get_setting("physics/3d/default_gravity")
-const ACCELERATION = 10
+const WALK_ACCELERATION = 10
 const DEACCELERATION = 10
+
+onready var MAX_SPRINT_SPEED = DEFAULT_MOVE_SPEED * 1.5
+const SPRINT_ACCELERATION = WALK_ACCELERATION * 1.5
+
+onready var CROUCH_MOVE_SPEED = DEFAULT_MOVE_SPEED * 0.6
 
 var target_direction = Vector3.ZERO
 var velocity = Vector3.ZERO
 var hvel = Vector3.ZERO
+
+var move_state = IDLE
 
 # -----------------------
 # Camera controls
@@ -53,8 +67,8 @@ func process_input(delta):
 	# -----------------------
 	# Movement
 	var input_vector = Vector2(
-			Input.get_action_strength("move_left") - Input.get_action_strength("move_right"),
-			Input.get_action_strength("move_forward") - Input.get_action_strength("move_backward"))
+			Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+			Input.get_action_strength("move_backward") - Input.get_action_strength("move_forward"))
 	input_vector = input_vector.normalized()
 	
 	# TODO: Add joystick movement input support
@@ -65,7 +79,18 @@ func process_input(delta):
 	# Jumping
 	if Input.is_action_just_pressed("move_jump") and is_on_floor():
 		velocity.y = JUMP_SPEED
-
+		pass
+		move_state = JUMP
+	
+	# Sprinting
+	if Input.is_action_pressed("move_sprint") and is_on_floor():
+		pass
+		move_state = SPRINT
+	
+	# Crouching
+	if Input.is_action_pressed("crouch"):
+		pass
+		move_state = CROUCH
 
 func _input(event):
 	# -----------------------
@@ -74,7 +99,7 @@ func _input(event):
 		# Rotate around self when moving in viewport x-plane
 		self.rotate_y(deg2rad(event.relative.x * MOUSE_SENSITIVITY * -1))
 		# Rotate camera about gimbal (target) when mouse moves in y-plane
-		camera_target.rotate_x(deg2rad(event.relative.y * MOUSE_SENSITIVITY))
+		camera_target.rotate_x(deg2rad(event.relative.y * MOUSE_SENSITIVITY * -1))
 		camera_target.rotation.x = clamp(camera_target.rotation.x, -deg2rad(70), deg2rad(90))
 
 
@@ -87,13 +112,24 @@ func process_movement(delta):
 	hvel.y = 0
 	
 	var target = target_direction
-	target *= DEFAULT_MOVE_SPEED
+	match move_state:
+		SPRINT:
+			target *= MAX_SPRINT_SPEED
+		CROUCH:
+			target *= MAX_SPRINT_SPEED
+		_:
+			target *= DEFAULT_MOVE_SPEED
 	
-	var acceleration
+	var acceleration = DEACCELERATION
+	
 	if target_direction.dot(hvel) > 0:
-		acceleration = ACCELERATION
-	else:
-		acceleration = DEACCELERATION
+		match move_state:
+			SPRINT:
+				acceleration = SPRINT_ACCELERATION
+			CROUCH:
+				acceleration = CROUCH_MOVE_SPEED
+			WALK:
+				acceleration = WALK_ACCELERATION
 	
 	hvel = hvel.linear_interpolate(target, acceleration * delta)
 	velocity.x = hvel.x
